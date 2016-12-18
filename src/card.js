8fcc4b54e552e59d309122e5c3acaafb685cac26
mantal <mantal0@outlook.fr>
2016-12-18 15:10:58 +0100

    Improve parsing

--- src/card.js
+++ src/card.js
@@ -1,16 +1,34 @@
 export function importCard(card) {
 	"use strict";
 
+	let m = card.match(/(^\w+):/);
+	const b = m ? importBoard(m[1]) : 'Mainboard';
+
+	let cardName = card.replace(/^\w+:/, '').trim();
+
+	m = cardName.match(/^\d+/g);
+	let n = m ? m[0] : 1;
+	if (n <= 0)
+		console.log('bad number: ', n);//TODO
+
+	return {
+		count: n,
+		board: b,
+		name: cardName.replace(/^\d+/, '').trim()
+	};
 }
 
 export function importBoard(board) {
 	"use strict";
-	switch (board.toLocaleLowerCase().replace(/ /g, '')) {
+	switch (board.toLocaleUpperCase().replace(/ /g, '')) {
 		case 'SB':
 			return 'Sideboard';
 		case 'MB':
 			return 'Maybeboard';
+		case 'CM':
 		case 'C':
 			return 'Commander';
+		default:
+			return board;
 	}
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,13 +2,12 @@
 	<div>
 		<textarea v-model.trim="deckListString" name="deck-list" id="deck-list" cols="30" rows="50"></textarea>
 		<div v-for="card in deckList.cards">
-			{{ card.board }}:  {{ card.count }} {{ card.name }}
+			{{ card.board }}: {{ card.count }} {{ card.name }}
 		</div>
 	</div>
 </template>
 
 <script>
-
 	import * as cards from '../card';
 
 	export default {
@@ -20,23 +19,7 @@
 		},
 		computed: {
 			deckList() {
-				let res = this.deckListString.split('\n').map(e => {
-					let m = e.match(/^(\w+):/);
-
-					const b = m ? m[0] : 'Mainboard';
-
-					e = e.replace(/^(\w+):/, '').trim();
-
-					m = e.match(/^\d+/g);
-					let n = m ? m[0] : 1;
-					if (n <= 0)
-						console.log('bad number: ', n);//TODO
-					return {
-						count: n,
-						board: b,
-						name: e.replace(/^\d+/, '').trim()
-					};
-				});
+				let res = this.deckListString.split('\n').map(cards.importCard);
 				return {
 					cards: res
 				}
